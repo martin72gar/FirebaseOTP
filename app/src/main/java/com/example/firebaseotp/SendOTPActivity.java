55d4a1f1f0696c5eb12e1bdf0f867b764521ea90
Martin on Forty's PC <72garmartin@gmail.com>
2021-06-16 14:08:19 +0700

    prepare for firebase

--- app/src/main/java/com/example/firebaseotp/SendOTPActivity.java
+++ app/src/main/java/com/example/firebaseotp/SendOTPActivity.java
@@ -2,7 +2,11 @@ package com.example.firebaseotp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SendOTPActivity extends AppCompatActivity {
 
@@ -10,5 +14,28 @@ public class SendOTPActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send_otp);
+
+        final EditText inputMobile = findViewById(R.id.edt_input_mobile);
+        Button btnGetOtp = findViewById(R.id.btn_get_otp);
+
+
+
+        btnGetOtp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String phoneNumber = inputMobile.getText().toString().trim();
+                if (phoneNumber.isEmpty()) {
+                    inputMobile.setError("Phone Number required");
+                    return;
+                }
+                if (phoneNumber.length() < 10) {
+                    inputMobile.setError("Phone number not valid");
+                    return;
+                }
+                Intent intent = new Intent(getApplicationContext(), VerifyOTPActivity.class);
+                intent.putExtra("mobile", phoneNumber);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/firebaseotp/VerifyOTPActivity.java
+++ app/src/main/java/com/example/firebaseotp/VerifyOTPActivity.java
@@ -3,12 +3,132 @@ package com.example.firebaseotp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class VerifyOTPActivity extends AppCompatActivity {
+
+    EditText inputCode1, inputCode2, inputCode3, inputCode4, inputCode5, inputCode6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_verify_otp);
+
+        TextView textMobile = findViewById(R.id.txt_mobile);
+        textMobile.setText(String.format(
+                "+62-%s", getIntent().getStringExtra("mobile")
+        ));
+
+        inputCode1 = findViewById(R.id.input_code1);
+        inputCode2 = findViewById(R.id.input_code2);
+        inputCode3 = findViewById(R.id.input_code3);
+        inputCode4 = findViewById(R.id.input_code4);
+        inputCode5 = findViewById(R.id.input_code5);
+        inputCode6 = findViewById(R.id.input_code6);
+
+        setupOTPInputs();
+
+    }
+
+    private void  setupOTPInputs() {
+        inputCode1.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(!s.toString().trim().isEmpty()) {
+                    inputCode2.requestFocus();
+                }
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        inputCode2.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(!s.toString().trim().isEmpty()) {
+                    inputCode3.requestFocus();
+                }
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        inputCode3.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(!s.toString().trim().isEmpty()) {
+                    inputCode4.requestFocus();
+                }
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        inputCode4.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(!s.toString().trim().isEmpty()) {
+                    inputCode5.requestFocus();
+                }
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        inputCode5.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(!s.toString().trim().isEmpty()) {
+                    inputCode6.requestFocus();
+                }
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
     }
 }
